4a4fd2564f09a38300ce230695480891dea69fbd
Marthe J. Neverdal <mjneverdal@outlook.com>
2022-09-23 16:50:41 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Card from '../components/card';
+import Card from '../components/Card';
 
 export default function Home() {
   return (
