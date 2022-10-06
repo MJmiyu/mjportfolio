29cb9f3b4430991006dd0b3bfe99f9539af4a207
Marthe J. Neverdal <mjneverdal@outlook.com>
2022-10-06 17:14:58 +0200

    added more cards and details for use are a real portfolio

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,79 @@ export default function Home() {
   return (
     <div>
       <h1 className={styles.Title}>My Portfolio</h1>
+      <p className={styles.Paragraph}>
+        Hello and welcome to my portfolio! My Name is Marthe Johanne Neverdal
+        and I have recently finished my front end developer studies. I have been
+        studying development from october 2018 to october 2022, while working
+        part time.<br></br> Learning programming has been fun and I'm always
+        looking to learn more! I hope you will find my journey interresting and
+        see all I have learned these years.
+      </p>
       <div className={styles.Cards}>
         <Card
           title="Dog API"
           description="A simple page where you can browse and search dogbreeds"
+          liveLink="http://miyuchan.net/dogapi"
           image="/images/dogbreed.png"
-          link="https://github.com/Noroff-FEU-Assignments/js-frameworks-course-assignment-MJmiyu"
+          link="https://github.com/MJmiyu/JSframework"
           linkName="JS Frameworks"
         />
-        <Card
-          title="Active Footwear"
-          description="Small e-commerce site selling shoes"
-          image="/images/activefootwear.png"
-          link="https://github.com/MJmiyu/semester_project2"
-          linkName="Semester Project 2"
-        />
+
         <Card
           title="Holidaze"
           description="Hotel booking and management site"
           liveLink="https://holidaze-opal.vercel.app/"
           image="/images/holidaze.png"
-          link="https://github.com/Noroff-FEU-Assignments/project-exam-2-MJmiyu"
+          link="https://github.com/MJmiyu/holidaze"
           linkName="Project Exam 2"
+        />
+        <Card
+          title="Letha"
+          description="Basic shoe shopping site"
+          liveLink="http://miyuchan.net/Letha/"
+          image="/images/letha.jpg"
+          link="https://github.com/MJmiyu/Letha"
+          linkName="Letha"
+        />
+        <Card
+          title="Home Decor"
+          description="Basic home decor shop"
+          liveLink="http://miyuchan.net/Homedecor/"
+          image="/images/home_decor.jpg"
+          link="https://github.com/MJmiyu/homedecor"
+          linkName="Homedecor"
+        />
+        <Card
+          title="Space"
+          description="Useage of public API"
+          liveLink="http://miyuchan.net/ProjectExam1/"
+          image="/images/project_exam_1.jpg"
+          link="https://github.com/MJmiyu/ProjectExam1"
+          linkName="Space"
+        />
+        <Card
+          title="Re-Cycle"
+          description="Small site with tour guides"
+          liveLink="http://miyuchan.net/recycle/"
+          image="/images/re_cycle.jpg"
+          link="https://github.com/MJmiyu/re-cycle"
+          linkName="Re-Cycle"
+        />
+        <Card
+          title="Red Pepper Pizza"
+          description="Small basic site with menu options"
+          liveLink="http://miyuchan.net/redpepperpizza/"
+          image="/images/red_pepper_pizza.jpg"
+          link="https://github.com/MJmiyu/redpepperpizza"
+          linkName="RedPepperPizza"
+        />
+        <Card
+          title="Styleguide"
+          description="small basic styleguide"
+          liveLink="http://miyuchan.net/designca2/"
+          image="/images/styleguide.jpg"
+          link="https://github.com/MJmiyu/styleguide"
+          linkName="Styleguide"
         />
       </div>
     </div>
